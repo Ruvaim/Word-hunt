004d4c113135092ae0c773fb70f042ffe6ebc0d1
Ruvaim <ruvaim51@gmail.com>
2022-10-01 23:09:09 +0530

    initial commit with api integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { Container } from '@mui/system';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [meanings, setMeanings] = useState([])
+  const [word, setWord] = useState("")
+
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/plane`)
+
+      setMeanings(data.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(meanings);
+
+
+  useEffect(() => {
+    dictionaryApi()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height:'100vh',backgroundColor:"#282c34",color:'white'}}>
+      <Container maxWidth="md" style={{display:"flex",flexDirection:"column",height:"100vh"}}>
+        Dictionary
+      </Container>
     </div>
   );
 }
