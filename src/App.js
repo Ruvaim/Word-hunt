f4393bbf5bbca5184415593677362a3e5f6329fc
Ruvaim <ruvaim51@gmail.com>
2023-06-08 23:04:46 +0530

    fixed warning in app js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-
 import { Container } from '@mui/system';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Definations from './Components/Definations/Definations';
 import Header from './Components/Header/Header';
 
 function App() {
-
-  const [meanings, setMeanings] = useState([])
-  const [word, setWord] = useState("")
-
-  const dictionaryApi = async() => {
-    try {
-      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-
-      setMeanings(data.data)
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  console.log(meanings);
-
+  const [meanings, setMeanings] = useState([]);
+  const [word, setWord] = useState('');
 
   useEffect(() => {
-    dictionaryApi()
+    const dictionaryApi = async () => {
+      try {
+        const data = await axios.get(
+          `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        );
+
+        setMeanings(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    dictionaryApi();
   }, [word]);
 
   return (
-    <div className="App" style={{height:'100vh',backgroundColor:"#282c34",color:'white'}}>
-      <Container maxWidth="md" style={{display:"flex",flexDirection:"column",height:"100vh"}}>
-        <Header 
-        word={word} 
-        setWord={setWord}/>
-        {meanings && (<Definations 
-        word={word}
-        meanings={meanings}
-        />)}
+    <div
+      className="App"
+      style={{ height: '100vh', backgroundColor: '#282c34', color: 'white' }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+      >
+        <Header word={word} setWord={setWord} />
+        {meanings && <Definations word={word} meanings={meanings} />}
       </Container>
     </div>
   );
